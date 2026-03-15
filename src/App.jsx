1c0ec33a18bcb982d5c31cfa600fab5aa52a128a
Joceline <jocesudigdo947@gmail.com>
2026-03-15 20:51:37 +0700

    move header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Header from "./components/Header";
 import FlashCardList from "./components/FlashCardList";
 import data from "./data/flashCards";
 import "./App.css";
@@ -6,7 +5,19 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <Header />
+      <div className="header">
+        <p className="jargon">From Beginner to Advanced Questions</p>
+        <div className="title">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+            alt="React Logo"
+            className="logo"
+          />
+          <h1>The React Flash Cards.</h1>
+        </div>
+        <p className="subtitle">Test your react knowledge by answering the flashcards below</p>
+      </div>
+      
       <FlashCardList cards={data} />
     </div>
   );
